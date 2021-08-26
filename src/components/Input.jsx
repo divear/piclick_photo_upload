b71e4a8016690ddcd4d89b18ad02106b1e797996
lukascobit <lukyn.odehnal333@gmail.com>
2021-08-26 09:53:54 +0200

    bug fixes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,11 +4,20 @@ import { storage } from './firebase/firebase'
 var storageRef = storage.ref("images")
 
 function Input() {
+    const [error, setError] = useState("")
+    var dataLen;
+    storageRef.listAll().then(u => dataLen = u.items.length);
     const [data, setData] = useState([])
-    const [photo, setPhoto] = useState()
+    var photo
     const [tempArray, setTempArray] = useState([])
-    function send(tr){
+    const [tempArray2, setTempArray2] = useState([])
+    function send(){
+        if(!photo){
+            setError("No photo provided")
+            return 
+        }
         const uploadTask = storage.ref(`images/${photo.name}`).put(photo)
+        console.log("photo: " + photo);
         uploadTask.on(
             "state changed",
             snapshot => {},
@@ -21,22 +30,27 @@ function Input() {
                     .child(photo.name)
                     .getDownloadURL()
                     .then(url =>{
-                        data.push(url)
-                        setData(data)
-                        console.log(data);
+                        tempArray2.push(url)
+                        setTempArray2(tempArray2)
+                        photo = ""
                     })
             }
         )
+        console.log(dataLen);
+        tempArray2.slice(0, dataLen)
+        setTempArray2(tempArray2)
+        setData(tempArray2)
+        console.log(data);
     }
     
     useEffect(() => {
-        console.log("dklfklasjklfjskaljflkasjdkljgksh");
         storageRef.listAll().then(u => (u.items.forEach(d => {
             d.getDownloadURL().then(url => {
                 tempArray.push(url)
                 setTempArray(data)
             })
         })))
+        tempArray.slice(0, dataLen)
         setData(tempArray)
     }, [data])
 
@@ -44,15 +58,18 @@ function Input() {
         <div>
             <img className="logo" src={piclick} alt="picklick" />
 
-            <input onChange={e=> setPhoto(e.target.files[0])} type="file" />
+            <input onChange={e=> photo = e.target.files[0]} type="file" />
             <button onClick={send} className="send">send</button>
+            <h1>{error}</h1>
 
+            <p>{data[0] && data.length}</p>
             <div className="posts">
                 {
                     data[0] && data.map((d, index )=>{
-                        // console.log(d);
                         return(
-                            <img key={index} className="post" src={d} alt="" />
+                            <div>
+                                <img key={index} className="post" src={d} alt="" />
+                            </div>
                         )
                     })
                 }
